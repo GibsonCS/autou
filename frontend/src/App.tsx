c0fbb521edac5cc90c952f5622b0a6c2586e46fe
GibsonCS <gibson.cruzds@gmail.com>
2025-08-31 19:23:01 -0300

    chore: change text info

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -24,22 +24,19 @@ export default function App() {
                   Classifique seus e-mails e ganhe tempo
                 </h1>
                 <p className="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8 dark:text-gray-400">
-                  Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure
-                  qui lorem cupidatat commodo. Elit sunt amet fugiat veniam
-                  occaecat.
+                  A Ferramenta de Análise e Classificação de E-mails foi
+                  desenvolvida para automatizar o processo de leitura,
+                  categorização e resposta de mensagens recebidas, oferecendo
+                  uma solução ágil e inteligente para equipes que lidam com
+                  grande volume de comunicação.
                 </p>
+
                 <div className="mt-10 flex items-center justify-center gap-x-6">
                   <a
-                    href="#"
+                    href="#form"
                     className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 dark:bg-indigo-500 dark:hover:bg-indigo-400 dark:focus-visible:outline-indigo-500"
                   >
                     Iniciar
-                  </a>
-                  <a
-                    href="#"
-                    className="text-sm/6 font-semibold text-gray-900 dark:text-white"
-                  >
-                    Saiba mais <span aria-hidden="true">→</span>
                   </a>
                 </div>
               </div>
