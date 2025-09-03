b2a9aecf1f1ac30a7e3f1ffc795ef4ecba3edf88
GibsonCS <gibson.cruzds@gmail.com>
2025-09-02 23:23:32 -0300

    feat: v1.0

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,27 @@
+from typing import Annotated
 import services
-from fastapi import FastAPI # type: ignore
+from fastapi import FastAPI, Form, UploadFile # type: ignore
+from fastapi.middleware.cors import CORSMiddleware # type: ignore
+from pathlib import Path
+
+origins = ["*"]
 
 app = FastAPI()
 
-@app.post("/api/email/classificar")
-async def handleEmail():
-    result = await services.classificarEmail('asds')
-    return result
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.post("/api/emails/classificar")
+async def handle_email(file: UploadFile, title: Annotated[str, Form()]):
+    arquivo = Path('./uploads/'+ file.filename)
+    arquivo.write_bytes(await file.read())
+
+    with open('./uploads/' + file.filename, 'r', encoding="utf-8") as arquivo:
+        contentFile = arquivo.read()
+    
+    data = services.classificar_email(contentFile)
+    return data
--- backend/services.py
+++ backend/services.py
@@ -1,6 +1,36 @@
+from dotenv import load_dotenv # type: ignore
+import nltk
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('punkt_tab')
+from nltk.corpus import stopwords # type: ignore
+from nltk.tokenize import word_tokenize # type: ignore
+from google import genai
+import os
+load_dotenv()
 
-async def classificarEmail(text):
-    email = {
-        "name": text
-    }
-    return email
+def classificar_email(text):
+
+    api_key = os.getenv("GEMINI_API_KEY")
+
+    tokens = word_tokenize(text.lower())
+        
+    stop_words = set(stopwords.words('portuguese'))
+    
+    tokens_filtrados = [palavra for palavra in tokens if palavra not in stop_words]
+
+    client =  genai.Client()
+
+    message = (
+    f"Classifique o texto a seguir como produtivo ou improdutivo no seguinte exemplo: 'Categoria: produtivo', 'Resposta automática: texto' "
+    f"Após a classificação, me informe uma resposta automática referente ao texto no campo resposta automática. "
+    f"Text: {tokens_filtrados}"
+)
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash", contents=message
+    )
+
+    arrayText = response.text.strip().split("\n")
+
+    return arrayText
